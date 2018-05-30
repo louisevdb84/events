049ac14b987a0d1c98ca2dab3b22fe7a123fcf6a
Louise van den Berg <louisevdb84@gmail.com>
2018-05-30 15:17:15 +0200

    add github pages

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "5040 Sanderlin Avenue #111, Memphis, TN, United States",
+        venue: "Memphis, TN, United States",
         imageURL: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be465b88fdf21a6e05ab522458452344&auto=format&fit=crop&w=1267&q=80"
     },   
    
@@ -16,7 +16,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "5040 Sanderlin Avenue #111, Memphis, TN, United States",
+        venue: "Memphis, TN, United States",
         imageURL: "https://images.unsplash.com/photo-1496024840928-4c417adf211d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94616ccf14e7ed3eb6fe9afeb953aec3&auto=format&fit=crop&w=1050&q=80"
     },   
     {
@@ -25,7 +25,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "Skatepark, Memphis, Tennessee, United States",
+        venue: "Memphis, Tennessee, United States",
         imageURL: "https://images.unsplash.com/photo-1471457704840-607b55bf1172?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b107f31d0358a3e91719f54fce650ca9&auto=format&fit=crop&w=1350&q=80"
     },  
     {
@@ -34,7 +34,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "330 Beale Street, Memphis, TN, United States",
+        venue: "Memphis, TN, United States",
         imageURL: "https://images.unsplash.com/photo-1502635385003-ee1e6a1a742d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=802159153b998fe99ae8c0bac1ebc204&auto=format&fit=crop&w=634&q=80"
     },   
     {
@@ -43,7 +43,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "5040 Sanderlin Avenue #111, Memphis, TN, United States",
+        venue: "Memphis, TN, United States",
         imageURL: "https://images.unsplash.com/photo-1478147427282-58a87a120781?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aee95d9fe8925d8a4ef01e22c04e714c&auto=format&fit=crop&w=1050&q=80"
     },   
     {
@@ -52,7 +52,7 @@ var eventList = [
         date: "Thu 24th May 19:00",
         startTime: "19:00",
         endTime: "21:00",
-        venue: "412 North Cleveland Street, Memphis, TN, United States",
+        venue: "Memphis, TN, United States",
         imageURL: "https://images.unsplash.com/photo-1468234847176-28606331216a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c82acc6a6472b4bfb4e5e4aa021f595e&auto=format&fit=crop&w=1315&q=80"
     },  
 ]
